9df64cba2d5f31be95dfd9c15b26dd343472d8a1
Amanda Nyander <amanda.nyander@hotmail.com>
2024-04-04 13:30:40 +0200

    Added first draft of server class

---
+++ src/ChatApp/ClientData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace server
+{
+	public class ClientData
+	{
+		string userID;
+		TcpClient tcpClient;
+
+		public ClientData(string userID, TcpClient tcpClient)
+		{
+			this.tcpClient = tcpClient;
+			this.userID = userID;
+		}
+	}
+}
+
