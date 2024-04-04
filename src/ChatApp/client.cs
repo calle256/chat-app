ca0591a0bfac9edea6b9095726547e20b51ba988
Amanda Nyander <amanda.nyander@hotmail.com>
2024-04-04 14:48:33 +0200

    Revert "revert commit testing"
    This reverts commit 8eeefef696225007c2fea5be0b0adc55e89ca9b7.

--- src/ChatApp/client.cs
+++ src/ChatApp/client.cs
@@ -1,4 +1,10 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 namespace client
 {
     public class Client
