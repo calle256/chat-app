6cfcea8d3bcf31dab59c38927a48c843fcd5b730
Calle Ovinder <calle.ovinder7@gmail.com>
2024-04-09 12:25:59 +0200

    Reorganized files and updated README with new build instructions

---
+++ ChatApp/client/SocketUtility.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatApp
+{
+    public static class SocketUtility
+    {
+        
+
+        public static void MsgSend(NetworkStream stream, string msg)
+        {
+
+            if (stream.CanWrite)
+            {
+                byte[] msgBuffer = Encoding.Default.GetBytes(msg); //omvandlar medelande till bytes
+                stream.Write(msgBuffer, 0, msgBuffer.Length); // skickar medelande
+            }
+
+        }
+
+        public static string MsgReceive(NetworkStream stream)
+        {
+            try
+            {
+                if (stream.CanRead)
+                {
+
+
+                    byte[] msgBuffer = new byte[1024]; // siffran går att ändra för vilken storlek man vill ha
+                    int received = stream.Read(msgBuffer, 0, msgBuffer.Length); // läser datan från stream
+
+                    if (received > 0)
+                    {
+                        return Encoding.Default.GetString(msgBuffer, 0, received);
+                    }
+                   
+                }
+                return string.Empty;
+            }
+            catch 
+            {
+                Console.Write("ERROR: Message not received!");
+                return "ERROR: Message not received!";
+            }
+
+        }
+    }
+}
