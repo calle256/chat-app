c62a1bdd63398e90544013033d21c8e5bb7695b4
Ghadi Khalil <ghadi-khalil@hotmail.com>
2024-04-12 16:57:17 +0200

    Edited HandleClient With session management

--- ChatApp/server/Server.cs
+++ ChatApp/server/Server.cs
@@ -6,13 +6,15 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ChatApp;
 using server;
 
 namespace server
 {
     public class Server
     {
-        static List<TcpClient> clients = new List<TcpClient>();
+        private readonly List<TcpClient> clients = new List<TcpClient>();
+        private readonly Send sender = new Send();
 
         public void StartServer()
         {
@@ -26,15 +28,56 @@ namespace server
                 TcpClient tcpClient = listener.start();
                 clients.Add(tcpClient);
 
-                Thread clientThread = new Thread(HandleClient);
+                Thread clientThread = new Thread(() => HandleClient(tcpClient));
                 clientThread.Start();
             }
         }
 
-        public static void HandleClient()
+        // Method to manage Client sessions
+        public void HandleClient(TcpClient client)
         {
-            //TODO: Implement send and receive classes to be able to communicate between server and client
-            return;
+            try
+            {
+                using(NetworkStream stream = client.GetStream())
+                {
+                    while(true)
+                    {
+                        string receivedMsg = SocketUtility.MsgReceive(stream);
+
+                        if(string.IsNullOrEmpty(receivedMsg))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Recieved: {0}", receivedMsg);
+
+                        // Send the message back to the client
+                        SocketUtility.MsgSend(stream, "Response: " + receivedMsg);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Client Handling Error!!: {0}", e.Message);
+            }
+
+            client.Close();
+            clients.Remove(client);
+        }
+
+        // method to broadcast message to all users
+        private void SendMsgToAll(string msg, TcpClient sender)
+        {
+            lock(clients)
+            {
+                foreach (var client in clients)
+                {
+                    if (client != sender)
+                    {
+                        var socket = client.Client;
+                        this.sender.MsgSend(socket, msg);
+                    }
+                }
+            }
         }
 
         //helper function for HandleDisconnect.
@@ -54,7 +97,7 @@ namespace server
 
         //Check connection status for each client every second and close streams for disconnected
         //clients.
-        public static void HandleDisconnect()
+        public void HandleDisconnect()
         {
             while (true)
             {
