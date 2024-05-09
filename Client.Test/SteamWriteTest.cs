4edc2c6e981b5b3b53623e4ba87d42121432cbc9
Deividas Malaska <malaska.q@gmail.com>
2024-05-09 23:39:53 +0200

    unit_test_deivi

---
+++ Client.Test/SteamWriteTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using ChatApp;
+using Moq;
+using System;
+using System.Net.Sockets;
+
+
+namespace Test
+{
+    [TestFixture]
+    public class SocketUtilityTest
+    {
+        [Test]
+        public void MsgSend_WhenStreamCanWrite_SendsMessage()
+        {
+            var mockStream = new Mock<Stream>(MockBehavior.Strict);
+            mockStream.Setup(s => s.CanWrite).Returns(true);
+            string msg = "Test message";
+            byte[] expectedBytes = Encoding.UTF8.GetBytes(msg);
+
+            mockStream.Setup(s => s.Write(expectedBytes, 0, expectedBytes.Length)).Verifiable();
+
+            SocketUtility.MsgSend(mockStream.Object, msg);
+
+            mockStream.Verify();
+        }
+
+        [Test]
+        public void MsgSend_WhenStreamCannotWrite_DoesNotSendMessage()
+        {
+            var mockStream = new Mock<Stream>(MockBehavior.Strict);
+            mockStream.Setup(s => s.CanWrite).Returns(false);
+            string msg = "Test message";
+
+
+
+            SocketUtility.MsgSend(mockStream.Object, msg);
+
+            mockStream.Verify(s => s.Write(It.IsAny<byte[]>(), 0, It.IsAny<int>()), Times.Never);
+        }
+
+    }
+}
